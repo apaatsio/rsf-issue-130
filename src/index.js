9f1b9222503604133f9bc6811d8dfb6d5bfbf780
Antti Ahti <antti.ahti@gmail.com>
2018-08-07 01:03:22 +0300

    Add redux-saga-firebase

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import ReduxSagaFirebase from 'redux-saga-firebase';
+
+new ReduxSagaFirebase();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
